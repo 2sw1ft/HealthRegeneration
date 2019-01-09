e686b562541c39f22c1447dff87682131de86eec
2sw1ft <46526226+2sw1ft@users.noreply.github.com>
2019-01-09 22:54:07 +0300

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,6 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("")]
+[assembly: Guid("9D7DA73F-1984-4193-BF08-C88B5CD50D80")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
